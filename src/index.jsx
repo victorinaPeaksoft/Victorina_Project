c7212e4b88cdbd6cfb81d0940f3671e349ffc586
Kydyrberdi uulu Baktyiar <kydyrberdiuulubaktyar@gmail.com>
2021-07-07 11:41:16 +0600

    create layout

--- src/index.jsx
+++ src/index.jsx
@@ -6,35 +6,16 @@ import "./bable";
 import React from 'react'
 import { render } from 'react-dom'
 import WebpackLogo from "@/assets/webpackLogo.png";
-import * as $ from "jquery";
-
+import App from './App'
 const post = new Post("Webpack Post Title", WebpackLogo);
 
-$("pre").addClass('code').html(post.toString());
 
-const App = () => (
-  <div className="container">
-    <h1>Webpack course</h1>
-    <hr />
-    <div className="logo"></div>
-    <hr />
-    <pre />
-    <hr />
-    <div className="box">
-      <h2>Less</h2>
-      <div className="card">
-        <h2>SCSS</h2>
-      </div>
-    </div>
-    <h1><i>Baktyiar </i></h1>
-  </div>
-
-
-)
 
 render(
-  <App />,
-  document.getElementById('root')
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 
